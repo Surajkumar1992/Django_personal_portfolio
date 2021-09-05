bc05059ad088fdf32578641e5f7dc0c8cf63eb8f
Surajkumar1992 <suraj.subham@gmail.com>
2021-09-05 11:51:59 +0530

    Added Blogs html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    datetime = models.DateTimeField(blank=True)
+    description = models.TextField(max_length=500)
