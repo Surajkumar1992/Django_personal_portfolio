953027ef27457600cbd881b66f96e7ab4ed9cd51
Surajkumar1992 <suraj.subham@gmail.com>
2021-10-01 13:50:44 +0530

    modified Settings File

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'portfolio',
 ]
 
@@ -121,10 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
